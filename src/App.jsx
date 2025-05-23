0a2f57627fe677bef1c1f994bca528541794bc95
MrSidSir <irshad1554@gmail.com>
2025-05-23 20:19:01 +0530

    create new pages and route it

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Search from './pages/Search';
+import CreateListing from './pages/CreateListing';
+import Profile from './pages/Profile';
 
-function App() {
- 
-
+export default function App (){
   return (
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+     <BrowserRouter>
+    <Routes>
+        <Route path='/' element ={<Home />} />
+        <Route path='/signIn' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
 
-export default App
+
+
+    </Routes>
+    </BrowserRouter>
+ 
+  )
+   
+}
